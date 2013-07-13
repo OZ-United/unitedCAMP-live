240959e839945fde03aa016aad76ceb4838cc065
Jan Antala <jan@antala.sk>
2013-07-14 00:10:22 +0200

    query - post request

--- live/app/scripts/services/Message.js
+++ live/app/scripts/services/Message.js
@@ -4,9 +4,9 @@ angular.module('liveApp')
 .factory('Message', ['$resource', function ($resource) {
   var origin = window.location.protocol + '//' + window.location.host;
   console.log(origin);
-  return $resource('http://united-camp-live.janantala.com/messages/:messageId', { messageId: '@messageId' }, {
+  return $resource('http://united-camp-live.janantala.com/messages/:query:messageId', { messageId: '@messageId' }, {
     'create' : { method: 'POST', params: { } },
-    'query'   : { method: 'GET', headers: { 'X-App-Origin': origin}, params: { from: '@from' }, isArray: true },
+    'query'   : { method: 'POST', headers: { 'X-App-Origin': origin}, params: { from: '@from', query: 'query' }, isArray: true },
     'update'  : { method: 'PUT', params: { } },
     'remove'  : { method: 'DELETE', params: { } },
     'delete'  : { method: 'DELETE', params: { } }

d6db79c1a01caa92c045bb725c65b54e9cdd43aa
Jan Antala <jan@antala.sk>
2013-07-13 23:07:48 +0200

    server url

--- live/app/scripts/services/Message.js
+++ live/app/scripts/services/Message.js
@@ -4,7 +4,7 @@ angular.module('liveApp')
 .factory('Message', ['$resource', function ($resource) {
   var origin = window.location.protocol + '//' + window.location.host;
   console.log(origin);
-  return $resource('http://united-camp-live.dev/messages/:messageId', { messageId: '@messageId' }, {
+  return $resource('http://united-camp-live.janantala.com/messages/:messageId', { messageId: '@messageId' }, {
     'create' : { method: 'POST', params: { } },
     'query'   : { method: 'GET', headers: { 'X-App-Origin': origin}, params: { from: '@from' }, isArray: true },
     'update'  : { method: 'PUT', params: { } },
